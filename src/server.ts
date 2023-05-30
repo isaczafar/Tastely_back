c9bae8046b6e827ac39c932d86b996b93f2c8f98
isaczafar <dario.zeeler@gmail.com>
2023-05-30 18:00:37 +0200

    blah blah

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { Database } from './database';
 import { Recipe } from '../models/Recipe';
 import dotenv from 'dotenv';
 import { Sequelize, ModelCtor } from 'sequelize';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -10,42 +11,71 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 const database = new Database();
-(async() => await database.connect())();
-// await database.connect();
+(async () => await database.connect())();
 
 const sequelize = new Sequelize(process.env.DATABASEURL!, {
   dialect: 'postgres',
-  ssl: true, 
+  ssl: true,
 });
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', async(req, res) => {
-  // res.send('Hello, World!');
-  res.send(await Recipe.findAll());
+app.get('/', async (req, res) => {
+  res.send('Hello, World!');
 });
 
-app.get('/recipes', async(req, res) => {
-  // res.send('Hello, World!');
-  res.send(await Recipe.findAll());
+app.get('/recipes', async (req, res) => {
+  try {
+    const recipes = await Recipe.findAll();
+    res.json(recipes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 app.post('/recipes', async (req, res) => {
   try {
-    console.log("test");
-    const { name, description, ingredients, instructions } = req.body;
-    console.log(req.body)
-
-    // const RecipeModel: ModelCtor<any> = sequelize.models.Recipe; 
+    const {
+      name,
+      description,
+      ingredients,
+      instructions,
+      cookTime,
+      servings,
+      image,
+      creator,
+    } = req.body;
 
     const recipe = await Recipe.create({
       name,
       description,
       ingredients,
       instructions,
+      cookTime,
+      servings,
+      image,
+      creator,
     });
 
     res.json(recipe);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/recipes/:id', async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+    const recipe = await Recipe.findByPk(recipeId);
+
+    if (!recipe) {
+      res.status(404).json({ error: 'Recipe not found' });
+    } else {
+      res.json(recipe);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
