bbd6f172f57feaebca3b9cce89ac7a736bf5d3fc
Andrea Högberg <andreahogberg@live.se>
2023-05-31 16:16:46 +0200

    La till koppling mellan backend och frontend, login/signin, inte klart

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,30 @@
-import express from 'express';
-import { Database } from './database';
+import express, { Request, Response } from "express"
+import { Database } from "./database"
 
-const app = express();
-const port = process.env.PORT || 8080;
+const app = express()
+const port = process.env.PORT || 8080
 
-app.use(express.json());
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.get("/", (req, res) => {
+  res.send("Hello, World!")
+})
+
+app.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body
+
+  res.json({ success: true })
+})
+
+app.post("/signup", async (req: Request, res: Response) => {
+  const { username, password } = req.body
+
+  res.json({ success: true })
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
 
-const database = new Database();
-database.connect();
+const database = new Database()
+database.connect()
